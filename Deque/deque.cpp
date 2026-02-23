c2d828a5bcafd324b569db59d0a6b86a45a6dba6
ZhBauyrzhan <zhnbauyrzhan@gmail.com>
2026-02-23 19:42:02 +0300

    Added some logic, need to recup idea of iterator??

--- Deque/deque.cpp
+++ Deque/deque.cpp
@@ -1,8 +1,73 @@
 #include "deque.h"
+
+#include <cassert>
+#include <cstddef>
+#include <stdexcept>
+
 namespace bauyr {
-template <typename T> Deque<T>::Deque()  {}
-template <typename T> Deque<T>::~Deque()  {}
+
+template <typename T>
+Deque<T>::Deque(const size_type size_, size_type chunk_number)
+    : size_(size_), chunk_number(chunk_number), blocks(new T*[chunk_number]) {
+  assert(chunk_number > 0UL);
+  for (size_type i = 0; i < chunk_number; ++i) {
+    blocks[i] = new T[CHUNK_SIZE];
+  }
+  first_chunk = blocks[chunk_number / 3];
+  last_chunk = blocks[chunk_number - 1];
 }
+
+template <typename T>
+Deque<T>::Deque() : Deque(size_ = 0, chunk_number = 1) {}
+
+template <typename T>
+Deque<T>::Deque(const size_type size_, const T& value)
+    : Deque(size_, (size_ + CHUNK_SIZE - 1) / CHUNK_SIZE) {
+  // TODO: Oprimize this
+  // Fill chunks that should be full
+  for (size_type i = 0; i < chunk_number - 1; ++i) {
+    for (size_t j = 0; j < CHUNK_SIZE; ++j) {
+      blocks[i][j] = value;
+    }
+  }
+  // Fill the rest
+  for (size_t j = 0; j < size_ % CHUNK_SIZE; ++j) {
+    blocks[chunk_number - 1][j] = value;
+  }
+}
+
+template <typename T>
+Deque<T>::~Deque() {
+  for (size_type i = 0; i < chunk_number; ++i) {
+    delete[] blocks[i];
+  }
+  delete[] blocks;
+}
+
+template <typename T>
+T& Deque<T>::at(size_type pos) {
+  if (check_range(pos)) return this->operator[](pos);
+  throw std::out_of_range("Out of range attempt");
+}
+
+template <typename T>
+const T& Deque<T>::at(size_type pos) const {
+  if (check_range(pos)) return this->operator[](pos);
+  throw std::out_of_range("Out of range attempt");
+};
+;
+
+template <typename T>
+size_t Deque<T>::size() const {
+  return size_;
+};
+
+template <typename T>
+bool Deque<T>::check_range(size_t pos) {
+  return pos < size_;
+}
+
+}  // namespace bauyr
 int main() {
-    bauyr::Deque<int> d;
+  bauyr::Deque<int> d;
 }
--- Deque/deque.h
+++ Deque/deque.h
@@ -1,23 +1,56 @@
 #ifndef DEQUE_H
 #define DEQUE_H
 
+#include <cstddef>
 #include <cstdio>
 #include <type_traits>
 namespace bauyr {
 template <typename T>
 class Deque {
+  using value_type = T;
+  using size_type = size_t;
+  using reference_type = value_type&;
+  using pointer_type = value_type*;
+
   static_assert((std::is_copy_assignable_v<T> && std::is_copy_constructible_v<T>) &&
                 "Deque support only CopyAssignable and CopyConstructible types");
-  static const size_t chunck_size{32};
+  static const size_type chunck_size{32};
+
+ private:
+  Deque(const size_type size_, size_type chunk_number);
 
  public:
+  // Iterators
   template <bool IsPrivate>
   class base_iterator {};
+
+  // Constructors
   Deque();
+  Deque(const size_type size_, const T& value);
   Deque(const Deque<T>& deque);
   ~Deque();
 
-  private:
+  // Operators
+  Deque& operator=(const Deque& other);
+
+  // Methods
+  T& at(size_type pos);
+  const T& at(size_type pos) const;
+
+  size_type size() const;
+  
+  T& operator[](size_type pos);
+  T& operator[](size_type pos) const;
+
+ private:
+  const size_type CHUNK_SIZE = 32;
+  size_t size_;
+  size_type chunk_number;
+  T** blocks;
+  T* first_chunk;
+  T* last_chunk;
+
+  bool check_range(size_t pos);
 };
 }  // namespace bauyr
 #endif  // end of DEQUE_H
